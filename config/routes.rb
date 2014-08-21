fdb8899b3b025713f8168731353cb512e71e5d17
Nabarun Banerjee <nbanerjee@kreeti.com>
2014-08-21 11:57:10 +0530

    sign out implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
-  resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
